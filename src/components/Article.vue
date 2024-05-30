6f257582aef94b03c3e14f8d9da2386b52efe8d8
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2024-05-30 14:52:39 +0700

    Tugas Pertemuan 6

---
+++ src/components/Article.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    <img
+      src="https://i.kym-cdn.com/entries/icons/original/000/049/019/ambatron.jpg"
+      alt=""
+      width="500"
+    />
+    <h1>Ini adalah komponen artikel</h1>
+    <p>Ini adalah isi paragraf</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+import Article from '../components/Article.vue'
+</script>
+
+<template>
+  <main>
+    <Article />
+  </main>
+</template>
